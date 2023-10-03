6d91505e12b9b5c8962e8f6df01b78b525ff8cf6
dev-miguel-resources <ingenieromiguelch@gmail.com>
2023-10-03 18:48:16 -0300

    completed bases of dependency injection and bdd configs in application.properties

--- src/main/java/com/tlaxcala/controller/PatientController.java
+++ src/main/java/com/tlaxcala/controller/PatientController.java
@@ -1,14 +1,48 @@
 package com.tlaxcala.controller;
 
-import com.tlaxcala.model.Patient;
-import com.tlaxcala.service.PatientService;
+//import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.tlaxcala.model.Patient;
+//import com.tlaxcala.repo.IPatientRepo;
+import com.tlaxcala.service.IPatientService;
+//import com.tlaxcala.service.PatientServiceImpl;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/patients") // -> buena práctica: la ruta padre siempre sea en plural
+@RequiredArgsConstructor
 public class PatientController {
 
-    private PatientService service = new PatientService();
+    // forma 1: private PatientService service = new PatientService();
+    // forma 2: private PatientServiceImpl service = new PatientServiceImpl();
+    // forma 3: singleton
+    //@Autowired
+    //private PatientServiceImpl service;
+    // forma 4: forma 4: utilizar como inyección la definición de la fachada directamente mediante constructor
+    /*private final IPatientService service;
 
+      public PatientController(IPatientService service) {
+        this.service = service;
+    }*/
+    // forma 5: utilizar como inyección una interfaz de manera directa
+    /*@Autowired
+    private IPatientService service;*/
+    // forma 6: utilizar como inyección una interfaz + una anotación
+    private final IPatientService service;
+
+    @GetMapping()
     public String sayHelloREST() {
         Patient patient = new Patient(1, "Rusell");
         return service.sayHelloLogic(patient);
-    } 
+    }
+
+    @GetMapping("/hello2")
+    public String sayHelloREST2() {
+        Patient patient = new Patient(2, "Luis");
+        return service.sayHelloLogic(patient);
+    }
 }
---
+++ src/main/java/com/tlaxcala/repo/IPatientRepo.java
@@ -0,0 +1,8 @@
+package com.tlaxcala.repo;
+
+import com.tlaxcala.model.Patient;
+
+public interface IPatientRepo {
+    
+    String sayHello(Patient patient);
+}
