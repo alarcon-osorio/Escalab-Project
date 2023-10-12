5979b04ac4bd4aae0fb4f49544f40f97df8c2f0a
dev-miguel-resources <ingenieromiguelch@gmail.com>
2023-10-12 18:23:31 -0300

    crud patient version 1 completed

--- src/main/java/com/tlaxcala/controller/PatientController.java
+++ src/main/java/com/tlaxcala/controller/PatientController.java
@@ -1,13 +1,21 @@
 package com.tlaxcala.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 //import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.tlaxcala.model.Patient;
 //import com.tlaxcala.repo.IPatientRepo;
@@ -27,6 +35,36 @@ public class PatientController {
     public ResponseEntity<List<Patient>> findAll() {
         List<Patient> list = service.findAll();
         return new ResponseEntity<>(list, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Patient> save(@RequestBody Patient patient) {
+        Patient obj = service.save(patient);
+        // localhost:8081/patients/1
+        // return new ResponseEntity<>(obj, HttpStatus.CREATED);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getIdPatient()).toUri();
+        // return ResponseEntity.created(location).build();
+        return ResponseEntity.created(location).body(obj);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Patient> findById(@PathVariable("id") Integer id) {
+        Patient obj = service.findById(id);
+        return new ResponseEntity<>(obj, HttpStatus.OK);
+    }
+
+    //@PutMapping("/{id}")
+    @PutMapping("/{id}")
+    //@PatchMapping("/{id}")
+    public ResponseEntity<Patient> update(@PathVariable("id") Integer id, @RequestBody Patient patient) {
+        Patient obj = service.update(patient, id);
+        return new ResponseEntity<>(obj, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Patient> delete(@PathVariable("id") Integer id) {
+        service.delete(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     // forma 1: private PatientService service = new PatientService();
