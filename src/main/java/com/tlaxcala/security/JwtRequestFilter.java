e60c96b7482070cea5f1f3cc8b84241b8e329f53
dev-miguel-resources <ingenieromiguelch@gmail.com>
2024-01-02 18:24:40 -0300

    first steps in spring security with jwt

---
+++ src/main/java/com/tlaxcala/security/JwtRequestFilter.java
@@ -0,0 +1,28 @@
+package com.tlaxcala.security;
+
+import java.io.IOException;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class JwtRequestFilter extends OncePerRequestFilter {
+
+    private final JwtTokenUtil jwtTokenUtil;
+    private final JwtUserDetailsService jwtUserDetailsService;
+    
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'doFilterInternal'");
+    } // para interceptar las peticiones http
+    
+}
--- src/main/java/com/tlaxcala/service/IPatientService.java
+++ src/main/java/com/tlaxcala/service/IPatientService.java
@@ -1,5 +1,8 @@
 package com.tlaxcala.service;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 //import java.util.List;
 
 import com.tlaxcala.model.Patient;
@@ -9,5 +12,7 @@ public interface IPatientService extends ICRUD<Patient, Integer> {
    // DRY Principle: Don't repeat yourself! X
    // method: obtenga los pacientes ingresados a la UCI
    //List<Patient> getUCIPatients();
+
+   Page<Patient> listPage(Pageable pageable);
 
 }
